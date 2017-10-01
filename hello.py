e248e47fa069b3f97627ecd984d07830d56a366b
Giovanna Blasco <giovanna.blasco@hotmail.com>
2017-10-01 19:35:27 -0300

    uploads diferentes agora vão para diretorios diferentes e em pastas com data e hora do upload

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_dropzone import Dropzone
+from datetime import datetime
 import os
 app = Flask(__name__)
 dropzone = Dropzone(app)
@@ -28,34 +29,43 @@ def upload():
         # se arquivos vem do dataset
         if(request.form['name'] == 'dataset'):
             print("Oi! Você fez upload do dataset!")
+
+            #pega data e hora atual
+            id = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
             # cria diretorio uploaded na pasta atual, se já não existir
-            if not os.path.exists(app.config['UPLOADED_PATH'] + '/dataset-uploaded'):
-                os.makedirs(app.config['UPLOADED_PATH'] + '/dataset-uploaded')
+            if not os.path.exists(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id):
+                os.makedirs(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id)
             # loop over files since we allow multiple files
             for f in request.files.getlist('file'):
                 # print para verificar erros no server
                 print(request.files)
                 #salva arquivos na pasta atual + /uploaded
-                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/dataset-uploaded', f.filename))
+                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id, f.filename))
 
             # chamar algoritmos aqui
 
         # se arquivos vem do partition
         if(request.form['name'] == 'partition'):
             print("Oi! Você fez upload do partitions!")
+
+            #pega data e hora atual
+            id = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
             # cria diretorio uploaded na pasta atual, se já não existir
-            if not os.path.exists(app.config['UPLOADED_PATH'] + '/partitions-uploaded'):
-                os.makedirs(app.config['UPLOADED_PATH'] + '/partitions-uploaded')
+            if not os.path.exists(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id):
+                os.makedirs(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id)
             # loop over files since we allow multiple files
             for f in request.files.getlist('file'):
                 # print para verificar erros no server
                 print(request.files)
                 #salva arquivos na pasta atual + /uploaded
-                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/partitions-uploaded', f.filename))
+                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id, f.filename))
 
             # chama o loadClusters.py aqui
 
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
