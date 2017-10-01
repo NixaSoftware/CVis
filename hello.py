20190c4eaf9e318cfa26c147aa28a23d321a0f23
Giovanna Blasco <giovanna.blasco@hotmail.com>
2017-10-01 17:59:13 -0300

    agora ta dando uploadddddd enfim

--- hello.py
+++ hello.py
@@ -5,23 +5,26 @@ app = Flask(__name__)
 dropzone = Dropzone(app)
 
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.txt, .clu'
 
 # get the current folder
-app.config['UPLOADED_PATH'] = os.getcwd() + '/upload'
+app.config['UPLOADED_PATH'] = os.getcwd()
 
 @app.route('/')
 def hello_world():
    return render_template('index.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+        data = request.data
          # loop over files since we allow multiple files
         for f in request.files.getlist('file'):
             print(request.files)
             f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
         # chamar algoritmos aqui
-    return "OK"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
