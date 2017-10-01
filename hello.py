8f7b93f4a0757af77d80403a83b85124128dc786
Giovana Morais <giovana.vmorais@gmail.com>
2017-10-01 13:42:24 -0300

    inserção de testes referentes à redireção de página após a submissão do gormulário. erro no upload na parte de diretórios.

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_dropzone import Dropzone
 import os
 app = Flask(__name__)
@@ -9,19 +9,27 @@ app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 # get the current folder
 app.config['UPLOADED_PATH'] = os.getcwd() + '/upload'
 
+# essa variável serve pra gente não ter que ficar rodando
+# o servidor de novo toda hora
+# por via das dúvidas dá um export nela tbm
+DEBUG = True
+
 @app.route('/')
-def hello_world():
+def index():
    return render_template('index.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    arqs = []
     if request.method == 'POST':
          # loop over files since we allow multiple files
         for f in request.files.getlist('file'):
             print(request.files)
             f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+            arqs.append(f)
         # chamar algoritmos aqui
-    return "OK"
+    return "arquivos {}.".format(arqs)
+    # return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
