ec22ca3ad27f88852fb88e509a5d9874a98e6ec7
Giovanna Blasco <giovanna.blasco@hotmail.com>
2017-10-01 11:37:41 -0300

    tentando fazer esse demonio de dropzone funcionar

--- hello.py
+++ hello.py
@@ -1,31 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
 import os
 app = Flask(__name__)
-
-app = Flask(__name__)
 dropzone = Dropzone(app)
 
-app.config.update(
-    UPLOADED_PATH=os.getcwd() + '../upload',
-    DROPZONE_ALLOWED_FILE_TYPE='text',
-    DROPZONE_INPUT_NAME='data',
-)
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 
-
+# get the current folder
+app.config['UPLOADED_PATH'] = os.getcwd() + '/upload'
 
 @app.route('/')
 def hello_world():
    return render_template('index.html')
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        f = request.files.get('data')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    return render_template('index.html')
-
+         # loop over files since we allow multiple files
+        for f in request.files.getlist('file'):
+            print(request.files)
+            f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+        # chamar algoritmos aqui
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
