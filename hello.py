d81cfe4f8d7bde70e1bce542cd8511f07924374c
Giovanna Blasco <giovanna.blasco@hotmail.com>
2017-10-11 19:45:01 -0300

    front quase pronto, verificar dados que estão indo pro hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_dropzone import Dropzone
 from datetime import datetime
+import json
 import os
 app = Flask(__name__)
 dropzone = Dropzone(app)
@@ -21,52 +22,88 @@ def index():
 def upload():
     # arqs = []
     if request.method == 'POST':
-
         # se arquivos vem do dataset
         if(request.form['name'] == 'dataset'):
-            print("Oi! Você fez upload do dataset!")
-            print(request.form['selAlgorithm'])
-            print(request.form['selMetric'])
-            print(request.form['allordist'])
-            print(request.form['minSize'])
-            print(request.form['maxSize'])
+            if(request.form['partition'] == 'yes'):
+                pass
+            else:
+                print(request.form['minK'])
+                print(request.form['maxK'])
 
-            #pega data e hora atual
-            id = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                #pega data e hora atual
+                id = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-            # cria diretorio uploaded na pasta atual, se já não existir
-            if not os.path.exists(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id):
-                os.makedirs(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id)
-            # loop over files since we allow multiple files
-            for f in request.files.getlist('file'):
-                # print para verificar erros no server
-                print(request.files)
-                #salva arquivos na pasta atual + /uploaded
-                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/dataset-uploaded/' + id, f.filename))
+                dir = '/uploaded-data/' + id
+                # cria diretorio uploaded na pasta atual, se já não existir
+                if not os.path.exists(app.config['UPLOADED_PATH'] + dir):
+                    os.makedirs(app.config['UPLOADED_PATH'] + dir)
 
-            # chamar algoritmos aqui
+                for f in request.files.getlist('file'):
+                    # print para verificar erros no server
+                    print(request.files)
+                    #salva arquivos na pasta atual + /uploaded
+                    f.save(os.path.join(app.config['UPLOADED_PATH'] + dir, f.filename))
+
+                # lê o numero de objetos do dataset
+                with open(app.config['UPLOADED_PATH'] + dir + '/' + f.filename) as f:
+                    print(sum(1 for _ in f)-1)
+
+                # chamar algoritmos
+                if(request.form['mocleSelected'] == 'yes'):
+                    # crossover
+                    # tratar: se M = 1 ou se B = 2
+                    print(request.form['tipoDistMocle'])
+                    # number of generations
+                    print(request.form['numGen'])
+                    # nearest neighbours
+                    print(request.form['nearNeigh'])
+
+                if(request.form['basicSelected'] == 'yes'):
+                    # crossover E ou P
+                    print(request.form['tipoDistBasic'])
+                    # algorithms (1, 2, 3, 4, 5, 6, 7)
+                    # se composto, vem com virgula
+                    print(request.form['alg'])
+
 
         # se arquivos vem do partition
         if(request.form['name'] == 'partition'):
-            print("Oi! Você fez upload do partitions!");
-            print(request.form['allordist'])
-            print(request.form['minSize'])
-            print(request.form['maxSize'])
 
             #pega data e hora atual
             id = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
+            dir = '/uploaded-part/' + id
+
             # cria diretorio uploaded na pasta atual, se já não existir
-            if not os.path.exists(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id):
-                os.makedirs(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id)
+            if not os.path.exists(app.config['UPLOADED_PATH'] + dir + '/partition'):
+                os.makedirs(app.config['UPLOADED_PATH'] + dir + '/partition')
+
+            if not os.path.exists(app.config['UPLOADED_PATH'] + dir):
+                os.makedirs(app.config['UPLOADED_PATH'] + dir)
+
+            for n in request.files.getlist('datasetfile'):
+                n.save(os.path.join(app.config['UPLOADED_PATH'] + dir, n.filename));
+
             # loop over files since we allow multiple files
             for f in request.files.getlist('file'):
-                # print para verificar erros no server
-                print(request.files)
                 #salva arquivos na pasta atual + /uploaded
-                f.save(os.path.join(app.config['UPLOADED_PATH'] + '/partitions-uploaded/' + id, f.filename))
+                f.save(os.path.join(app.config['UPLOADED_PATH'] + dir + '/partition' , f.filename));
 
-            # chama o loadClusters.py aqui
+            # checa numero de arquivos na pasta para saber se é o ultimo post
+            qtinfolder = len([name for name in os.listdir(app.config['UPLOADED_PATH'] + dir + '/partition') if os.path.isfile(os.path.join(app.config['UPLOADED_PATH'] + dir + '/partition', name))])
+
+            # compara numero de arquivos na pasta com numero de arquivos aceitos no dropzone
+            if(qtinfolder == int(request.form['qtofdata'])):
+                print("Oi! Você fez upload do partitions!");
+                if(request.form['mocleSelected'] == 'yes'):
+                    # tratar: se M = 1 ou se B = 2
+                    print(request.form['tipoDistMocle'])
+                    # number of generations
+                    print(request.form['numGen'])
+                    # nearest neighbours
+                    print(request.form['nearNeigh'])
+
+                    # chama o loadClusters.py aqui
 
     return render_template('index.html')
 
